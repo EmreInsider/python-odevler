67d849733bfb3256a461bc4d70019e6567ff92a6
EmreInsider <58817387+EmreInsider@users.noreply.github.com>
2021-08-11 14:13:27 +0300

    Add files via upload

---
+++ ödev-5.py
@@ -0,0 +1,28 @@
+
+1- $('.in-check-box-wrapper')
+By.className('in-check-box-wrapper')
+
+2-$('#save-and-next')
+By.id('save-and-next')
+$('.in-text-input-wrapper__error-message')
+By.className('in-text-input-wrapper__error-message')
+
+3-$('#search-value')
+By.id('search-value')
+
+4-$('.in-radio-button-wrapper__label')[5]
+By.className('in-radio-button-wrapper__label')
+
+5-By.partialLinkText('/journey-builder/1091/overall-statistics')
+
+6-$('.in-button-wrapper_text-alert')
+By.className('in-button-wrapper_text-alert')
+
+7- By.partialLinkText('/sprite.svg#line-change')
+By.partialLinkText('/sprite.svg#line-delete')
+
+8-$('.in-toasts-wrapper')
+By.className('in-toasts-wrapper')
+
+9-$('#create-mobile-campaign')
+By.id('create-mobile-campaign')
